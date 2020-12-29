9d77c9eb56dda33780c76228577b0b4edfbf702f
Chuanqi Shi <shicq01@gmail.com>
2020-12-29 18:36:58 +0800

    Update io_demo.js
    添加按语种过滤信息

--- wiki/io_demo.js
+++ wiki/io_demo.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const LANGUAGE = {'zh': 1, 'zh-hant': 2, 'en': 3, 'ru': 4, 'ja': 5};
+
 const rl = readline.createInterface({
   // input: process.stdin,
-  // input: fs.createReadStream('data/wiki_en.xml'),
-  input: fs.createReadStream('E:/bzip2/latest-all.json', {start: 0}),   // 可以通过修改start的数值来指定读取位置
+  input: fs.createReadStream('../data/10.json'),
+  // input: fs.createReadStream('E:/bzip2/latest-all.json', {start: 0}),   // 可以通过修改start的数值来指定读取位置
   crlfDelay: Infinity
 });
 
-const os = fs.createWriteStream('../data/1.json');
+const os = fs.createWriteStream('../data/target.json');
 
 var lineNo = 0;
 var index = 0;
@@ -16,11 +18,13 @@ var index = 0;
 // readline读到一个\n或\r之后的操作，line是读到\n或\r之前的所有内容
 rl.on('line', (line) => {
   lineNo++;
-  if (lineNo <= 10) {
+  if (lineNo <= 1) {
     os.write(line);
-    index += line.length
+    index += line.length;
     try {
-      console.log(JSON.parse(line.slice(0, -1)))
+      // var jsonObj = JSON.parse(line.slice(0, -1));
+      var jsonObj = JSON.parse(line);
+      languageFilter(jsonObj[0]);
     } catch (e) {
       console.log('JSON Parsing Error!')
       console.log(e)
@@ -35,3 +39,37 @@ rl.on('close', () => {
   console.log(`readline closed, read ${lineNo} lines.`);
   console.log(`cursor at: ${index + 1}`)
 });
+
+
+
+function languageFilter(entity) {
+  
+  result = {};
+
+  for (let key of Object.keys(entity)) {
+    result[key] = {};
+    console.log(key);
+  }
+
+  result['type'] = entity['type'];
+  result['id'] = entity['id'];
+
+  for (let lang of Object.keys(LANGUAGE)) {
+    result['labels'][lang] = entity['labels'][lang];
+    result['descriptions'][lang] = entity['descriptions'][lang];
+    result['aliases'][lang] = entity['aliases'][lang];
+    // result['sitelinks']
+  }
+  
+  // result['descriptions'] = {}
+
+  // console.log(`Entity type: ${entity['type']}`);
+  // console.log(`Entity id: ${entity['id']}`);
+  // console.log(`Entity labels: ${entity['labels']}`);
+  // console.log(entity['labels']['zh'])
+  // console.log(entity['descriptions']);
+  // console.log(`Entity aliases: ${entity['aliases']}`);
+  console.log(entity['sitelinks'])
+  // console.log(result)
+}
+
