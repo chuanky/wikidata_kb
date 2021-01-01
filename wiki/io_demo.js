3c39dc2a50598b502b1d2caee61a7487b033b50f
chuanky <shicq01@gmail.com>
2021-01-01 12:35:28 +0800

    Update io_demo.js
    更新目标声明

--- wiki/io_demo.js
+++ wiki/io_demo.js
@@ -4,24 +4,26 @@ const readline = require('readline');
 const LANGUAGES = {'zh-hans': 1, 'zh-hant': 2, 'en': 3, 'ru': 4, 'ja': 5};
 const LANGUAGES_SITE = {'zhwiki': 1, 'zh_yuewiki': 2, 'enwiki': 3, 'ruwiki': 4, 'jawiki': 5};
 const CLAIMS = {
-  // for classification
-  'P279': 'subclass of', 
-  // for person
-  'P18': 'image',
-  'P27': 'country of citizenship', 
-  'P31': 'instance of',
-  'P36': 'capital',   // 如果地点是国家，需要找到该国家首都
-  'P39': 'position held',
-  'P102': 'member of political party',
-  'P569': 'date of birth',
-  // for location
-  'P17': 'country',   // 属于哪个国家
-  'P625': 'coordinate location',
-  'P1448': 'official name',
-  // for organization
+  // person: p; location: l; organizations: o
+  'P17': 'country',   // l, o: countryId
+  'P18': 'image', // p, l, o: photoUrl
+  'P27': 'country of citizenship', // p: countryId
+  'P31': 'instance of', // l: level, o: type
+  'P36': 'capital',   // l: capitalId
+  'P39': 'position held', // p: jobTitle_en, jobTitle_zh
+  'P102': 'member of political party',  // p: partyId, partyName
+  'P112': 'founded by', // o: founderId, founderName_en, founderName_zh
+  'P159': 'headquarters location',  // o: locationId, longitude, latitude
+  'P279': 'subclass of', // for classification
+  'P373': 'Commons category',  // o: category
+  'P488': 'chairperson',  // o: leaderId, leaderName_en, leaderName_zh
+  'P569': 'date of birth',  // p: birthday
+  'P571': 'inception',  // o: foundingDate
+  'P625': 'coordinate location',  // l, o: longitude, longitudeFloat, latitude, latitudeFloat
+  'P1448': 'official name', // l: ancestorNames
 }
 
-// TODO: location: level、parentId、category？
+
 
 
 class EntityProcessor {
