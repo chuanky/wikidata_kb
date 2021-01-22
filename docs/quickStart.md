5a0120732552a28eee0ba6a12167f9b14a574db0
chuanky <shicq01@gmail.com>
2021-01-22 12:53:14 +0800

    更新项目结构和文档

--- docs/quickStart.md
+++ docs/quickStart.md
@@ -1,8 +1,8 @@
 ## 项目使用流程
->> nodejs version: v12.18.1
-1. 下载wikidata JSON dump文件
+>nodejs version: v12.18.1
+1. 下载wikidata JSON dump文件。
 2. 使用json_dump_processor.js的JSONDumpProcessor类处理JSON dump文件，文件大小：1.1T -> 115G，文件过滤后变为JSON Line文件。
-3. 使用main.js根据Claims筛选出目标实体，运行过后生成3个文件，分别对应人物、组织、地点实体。
+3. 使用classify_entity.js根据Claims筛选出目标实体，运行过后生成3个文件，分别对应人物、组织、地点实体。
 4. 使用entity_matcher.js匹配wikidata和iricaDB中的实体。
 5. 使用matched_{per/org/loc}_processor.js[统计](知识库可更新实体统计-2020-01-19.md)和[更新](知识库更新数据统计_2020-01-21.md)的数据库字段。
 6. 使用matched_{per/org/loc}_processor.js中的processSingle函数进行单个实体更新。
--- wiki/classify_entity.js
+++ wiki/classify_entity.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const JSONUtil = require('../util/json_util')
-const EntityClassifier = require('../wiki/entity_classifier')
+const EntityClassifier = require('./entity_classifier')
 /**
  * 从wikidata jl格式文件中筛选出person、organization和location
  * 数据分别输出到三个jl文件当中
@@ -14,16 +14,17 @@ const EntityClassifier = require('../wiki/entity_classifier')
       ?item wdt:P279* wd:Q12047392.
       SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
     }
+ * 下载JSON格式数据后使用JSONUtil.loadSubclassesSync加载子类信息用于判断实体类别
  */
 
 // load classes resources
 let locations = JSONUtil.loadSubclassesSync('../data/locations.json');
-let gov_agencies = JSONUtil.loadSubclassesSync('../data/organizations.json');
+let organizations = JSONUtil.loadSubclassesSync('../data/organizations.json');
 // read write streams
 const rl = readline.createInterface({
   input: fs.createReadStream('../data/output-2020-12-28.jl')
 })
-
+// create write stream for per/org/loc
 const os_per = fs.createWriteStream('../data/output_per-2020-12-28.jl')
 const os_org = fs.createWriteStream('../data/output_org-2020-12-28.jl')
 const os_loc = fs.createWriteStream('E:/wikidata/output_loc-2020-12-28.jl')
@@ -35,7 +36,7 @@ rl.on('line', (line) => {
   let entity = JSON.parse(line);
   let entityStr = JSON.stringify(entity) + '\n';
   if (classifier.isPerson(entity)) os_per.write(entityStr);
-  if (classifier.isOrganization(entity, gov_agencies)) os_org.write(entityStr);
+  if (classifier.isOrganization(entity, organizations)) os_org.write(entityStr);
   if (classifier.isLocation(entity, locations)) os_loc.write(entityStr);
 
   if (counter % 100000 == 0) {
